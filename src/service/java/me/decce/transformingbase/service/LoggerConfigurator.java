1313d46d7fafd3040d08c275128894d6767e2cd7
decce6 <decce6@proton.me>
2026-06-26 13:56:42 +0800

    Fix early logger filtering

--- src/service/java/me/decce/transformingbase/service/LoggerConfigurator.java
+++ src/service/java/me/decce/transformingbase/service/LoggerConfigurator.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.impl.Log4jContextFactory;
 
 import java.util.List;
@@ -46,26 +47,19 @@ public class LoggerConfigurator {
             before = LoggerTester.testAll();
         }
 
-        configureDebugLog();
+        // Note: if logger was created before this, the original would still be used even after we configure the new
+        // async logger factory. Therefore, we configure both the original root logger and the async one.
+        // An example of what this fixes is the "Exiting event polling thread" message from Ixeris.
+        configureRootLogger();
 
         var selector = new BasicAsyncLoggerContextSelector();
         LogManager.setFactory(new Log4jContextFactory(selector));
-        if (AsyncLogger.config.filtering) {
-            // https://logging.apache.org/log4j/2.x/manual/filters.html#filtering-process
-            if (AsyncLogger.config.filterGlobal) {
-                // Filter in the "Logger" stage (earliest stage)
-                LoggerContext.getContext(false).addFilter(new AsyncFilter());
-            }
-            else {
-                // Filter in the "LoggerConfig" stage (2nd stage, earliest after LogEvent creation)
-                ((Logger) LogManager.getRootLogger()).addFilter(new AsyncFilter());
-            }
-        }
+
+        configureRootLogger();
+
         if (AsyncLogger.config.wrapSysOutSysErr) {
             configureSysOutErr();
         }
-
-        configureDebugLog();
 
         var logger = LogManager.getLogger(Constants.MOD_NAME);
         logger.info("Successfully configured async logger context with [wrapSysOutSysErr={}, filtering.enabled={}, filtering.global={}]", AsyncLogger.config.wrapSysOutSysErr, AsyncLogger.config.filtering, AsyncLogger.config.filterGlobal);
@@ -83,10 +77,29 @@ public class LoggerConfigurator {
         }
     }
 
-    private static void configureDebugLog() {
+    private static void configureRootLogger() {
+        var root = LoggerContext.getContext(false).getConfiguration().getRootLogger();
+        configureDebugLog(root);
+        configureFilter();
+    }
+
+    private static void configureDebugLog(LoggerConfig loggerConfig) {
         if (AsyncLogger.config.noDebugLog) {
-            var configuration = LoggerContext.getContext(false).getConfiguration();
-            configuration.getRootLogger().removeAppender("DebugFile");
+            loggerConfig.removeAppender("DebugFile");
+        }
+    }
+
+    private static void configureFilter() {
+        if (AsyncLogger.config.filtering) {
+            // https://logging.apache.org/log4j/2.x/manual/filters.html#filtering-process
+            if (AsyncLogger.config.filterGlobal) {
+                // Filter in the "Logger" stage (earliest stage)
+                LoggerContext.getContext(false).addFilter(new AsyncFilter());
+            }
+            else {
+                // Filter in the "LoggerConfig" stage (2nd stage, earliest after LogEvent creation)
+                ((Logger) LogManager.getRootLogger()).addFilter(new AsyncFilter());
+            }
         }
     }
 
